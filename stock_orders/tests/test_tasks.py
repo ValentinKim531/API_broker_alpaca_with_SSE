1b885e76f29bc96461234a58bfd6a88e0a9ed395
Valentin Kim <valentink@yandex.ru>
2024-01-31 08:39:24 +0600

    корректировка test_tasks.py

--- stock_orders/tests/test_tasks.py
+++ stock_orders/tests/test_tasks.py
@@ -1,22 +1,29 @@
 import pytest
+from unittest.mock import patch
 from celery.contrib.testing.worker import start_worker
 from backend.celery import app
 from stock_orders.tasks import listen_to_alpaca_sse
-from unittest.mock import patch
 
 
+# Фикстуры Celery
 @pytest.fixture(scope="module")
 def celery_app():
-    app.conf.update(task_always_eager=True, task_eager_propagates=True)
+    app.conf.update(
+        task_always_eager=True,
+        task_eager_propagates=True,
+        broker_url="memory://",
+        backend="cache+memory://",
+    )
     return app
 
 
 @pytest.fixture(scope="module")
 def celery_worker(celery_app):
-    with start_worker(celery_app, perform_ping_check=False):
-        yield
+    with start_worker(celery_app, perform_ping_check=False) as worker:
+        yield worker
 
 
+# Тесты
 @pytest.mark.django_db
 @patch("stock_orders.tasks.AlpacaSSEService")
 @patch("stock_orders.tasks.logger")
@@ -29,8 +36,6 @@ def test_listen_to_alpaca_sse_integration(
     listen_to_alpaca_sse.delay()
 
     mock_alpaca_service_instance.listen_events.assert_called_once()
-
-    # Проверка записи логов о запуске задачи и успешном подключении
     mock_logger.info.assert_any_call("Задача SSE запущена")
     mock_logger.info.assert_any_call("Подключение к SSE-серверу успешно")
 
@@ -45,19 +50,32 @@ def test_listen_to_alpaca_sse_integration(
 
 @patch("stock_orders.tasks.AlpacaSSEService")
 @patch("stock_orders.tasks.logger")
-def test_listen_to_alpaca_sse_success(mock_logger, mock_service):
+def test_listen_to_alpaca_sse_success(
+    mock_logger, mock_alpaca_service, celery_app
+):
+    mock_alpaca_service_instance = mock_alpaca_service.return_value
+    mock_alpaca_service_instance.listen_events.return_value = None
+
     listen_to_alpaca_sse()
-    mock_service.assert_called_once()
-    mock_service().listen_events.assert_called_once()
+
+    mock_alpaca_service_instance.listen_events.assert_called_once()
     mock_logger.info.assert_any_call("Задача SSE запущена")
     mock_logger.info.assert_any_call("Подключение к SSE-серверу успешно")
 
 
 @patch("stock_orders.tasks.AlpacaSSEService")
 @patch("stock_orders.tasks.logger")
-def test_listen_to_alpaca_sse_failure(mock_logger, mock_service):
-    mock_service().listen_events.side_effect = Exception("Test error")
+def test_listen_to_alpaca_sse_failure(
+    mock_logger, mock_alpaca_service, celery_app
+):
+    mock_alpaca_service_instance = mock_alpaca_service.return_value
+    mock_alpaca_service_instance.listen_events.side_effect = Exception(
+        "Test error"
+    )
+
     listen_to_alpaca_sse()
+
+    mock_alpaca_service_instance.listen_events.assert_called_once()
     mock_logger.error.assert_called_with(
         "Ошибка при подключении к SSE-серверу: Test error"
     )
